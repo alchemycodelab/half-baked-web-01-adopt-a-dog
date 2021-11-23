2db5d66236530f4cc3476bc8c6b97fde0394b286
Danny Cairns <dpcairns@gmail.com>
2021-11-22 16:19:14 -0800

    Update detail.js

--- detail/detail.js
+++ detail/detail.js
@@ -3,6 +3,7 @@ import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
-// on load// get the id from URL
+// on load
+// get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
